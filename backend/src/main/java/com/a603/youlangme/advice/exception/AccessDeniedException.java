deb3e9db9f437cf01ab1ceafba7ec26a021e02f4
minholee97 <l2minep@gmail.com>
2022-07-19 11:29:25 +0900

    init project

---
+++ backend/src/main/java/com/a603/youlangme/advice/exception/AccessDeniedException.java
@@ -0,0 +1,15 @@
+package com.a603.youlangme.advice.exception;
+
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException() {
+        super();
+    }
+
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
