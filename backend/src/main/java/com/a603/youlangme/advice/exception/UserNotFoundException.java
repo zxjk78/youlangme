e021c0b571625923d0c2e4d3b3fe76ed56bfbb4d
kms940125 <kms940125@hanmail.net>
2022-07-22 10:17:35 +0900

    issue: delete backend

--- backend/src/main/java/com/a603/youlangme/advice/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.a603.youlangme.advice.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException() {
-        super();
-    }
-}
--- backend/src/main/java/com/a603/youlangme/dto/follow/FollowRegisterRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.a603.youlangme.dto.follow;
-
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-public class FollowRegisterRequestDto {
-    private Long userIdToFollow;
-}
