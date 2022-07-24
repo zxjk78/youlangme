5d0d7a6785bdf83eb2c84f16cd00dd822169d6be
noh yongrae <zxjk78@gmail.com>
2022-07-25 08:57:33 +0900

    commit before merge

--- frontend/src/features/auth/modify/modifyAPI.js
+++ frontend/src/features/auth/modify/modifyAPI.js
@@ -3,8 +3,14 @@ const API_URL = 'http://127.0.0.1:8080/';
 // 리덕스랑 관련없는 서버 통신 API들 모음
 // chips에 담을 hobby 데이터 fetch
 export const fetchHobbies = async (setHobbiesFn) => {
-  console.log('fetch 취미');
-  const response = await axios.get(API_URL + `favorite/list`);
+  // console.log('fetch 취미');
+  const user = JSON.parse(localStorage.getItem('user'));
+  const accessToken = user ? user.accessToken : null;
+  const response = await axios.get(API_URL + `favorite/list`, {
+    headers: {
+      'X-Auth-Token': accessToken,
+    },
+  });
   console.log(response);
   setHobbiesFn(
     response.data.data.map((item) => {
