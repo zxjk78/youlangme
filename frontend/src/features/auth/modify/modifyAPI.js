cdd93b4bd0582c48ca1d760a201cc32fec52ee30
eunminbb <mimi0207ej@naver.com>
2022-07-24 22:55:59 +0900

    Fix: Fetch favorite in modify - include accessToken when GET request

--- frontend/src/features/auth/modify/modifyAPI.js
+++ frontend/src/features/auth/modify/modifyAPI.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 const API_URL = 'http://127.0.0.1:8080/';
+
+const user = JSON.parse(localStorage.getItem('user'));
+const accessToken = user ? user.accessToken : null;
+const getConfig = { headers: { 'X-Auth-Token': accessToken } };
+
+
 // 리덕스랑 관련없는 서버 통신 API들 모음
 // chips에 담을 hobby 데이터 fetch
 export const fetchHobbies = async (setStateFn) => {
   console.log('fetch 취미');
-  const response = await axios.get(API_URL + `favorite/list`);
+  // const response = await axios.get(API_URL + `favorite/list`);
+  const response = await axios.get(
+    API_URL + `favorite/list`,
+    // 엑세스 토큰이 필요하다.
+    getConfig
+  );
   console.log(response);
   setStateFn(
     response.data.data.map((item) => {
