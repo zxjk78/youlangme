a072880dc93c003949cf2ce19c82b3f95ed0f5d6
kms940125 <kms940125@hanmail.net>
2022-07-25 11:46:20 +0900

    feat:currentUser request

--- frontend/src/features/main/Main.js
+++ frontend/src/features/main/Main.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { logout } from "../auth/authSlice";
+import { getUser, logout } from "../auth/authSlice";
 const Main = () => {
   const { isLoggedIn } = useSelector((state) => state.auth);
+  const { currentUser } = useSelector((state) => state.auth)
   const dispatch = useDispatch();
   const history = useHistory();
+  useEffect( () => {
+   dispatch(getUser()) 
+   if (!currentUser.name){
+    history.push('/modify')
+   }
+  }, [dispatch, history, currentUser])
   const logoutHandler = () => {
     dispatch(logout());
     history.push("/");
   };
+  // if (currentUser.name == null){
+  //   history.push('/modify')
+  // }
   console.log(isLoggedIn);
+  console.log(currentUser)
   // const { user } = useSelector((state) => state.auth);
   // console.log(user);
   return (
     <div>
       <h2>이곳은 임시 홈페이지</h2>
-      <p>{isLoggedIn}</p>
+      
       <button onClick={logoutHandler}>로그아웃</button>
     </div>
   );
