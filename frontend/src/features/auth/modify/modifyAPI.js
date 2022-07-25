f3f98d4be50dcfda245c37c83657d0cce15e9ea0
eunminbb <mimi0207ej@naver.com>
2022-07-25 09:28:45 +0900

    Fix: fix variable name in modify API

--- frontend/src/features/auth/modify/modifyAPI.js
+++ frontend/src/features/auth/modify/modifyAPI.js
@@ -9,7 +9,7 @@ const getConfig = { headers: { 'X-Auth-Token': accessToken } };
 // 리덕스랑 관련없는 서버 통신 API들 모음
 // chips에 담을 hobby 데이터 fetch
 
-export const fetchHobbies = async (setStateFn) => {
+export const fetchHobbies = async (setHobbiesFn) => {
   console.log('fetch 취미');
   // const response = await axios.get(API_URL + `favorite/list`);
   const response = await axios.get(
