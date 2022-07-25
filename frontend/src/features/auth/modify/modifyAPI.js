83b6dca7863cd1bdc80b44209fc45ce3d3af2461
eunminbb <mimi0207ej@naver.com>
2022-07-25 14:56:03 +0900

    Feat: Upload and fetch profile image in Profile page

--- frontend/src/features/auth/modify/modifyAPI.js
+++ frontend/src/features/auth/modify/modifyAPI.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 const API_URL = 'http://127.0.0.1:8080/';
 
-const user = JSON.parse(localStorage.getItem('user'));
-const accessToken = user ? user.accessToken : null;
-const getConfig = { headers: { 'X-Auth-Token': accessToken } };
+
 
 // 리덕스랑 관련없는 서버 통신 API들 모음
 // chips에 담을 hobby 데이터 fetch
 
 export const fetchHobbies = async (setHobbiesFn) => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const accessToken = user ? user.accessToken : null;
+  const getConfig = { headers: { 'X-Auth-Token': accessToken } };
+  
   console.log('fetch 취미');
   // const response = await axios.get(API_URL + `favorite/list`);
   const response = await axios.get(
