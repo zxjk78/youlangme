fbc3f4060cfea7167b9702796f306ac2f9c40d08
noh yongrae <zxjk78@gmail.com>
2022-07-25 09:25:04 +0900

    solve conflict

--- frontend/src/features/auth/modify/modifyAPI.js
+++ frontend/src/features/auth/modify/modifyAPI.js
@@ -5,11 +5,10 @@ const user = JSON.parse(localStorage.getItem('user'));
 const accessToken = user ? user.accessToken : null;
 const getConfig = { headers: { 'X-Auth-Token': accessToken } };
 
-
 // 리덕스랑 관련없는 서버 통신 API들 모음
 // chips에 담을 hobby 데이터 fetch
 
-export const fetchHobbies = async (setStateFn) => {
+export const fetchHobbies = async (setHobbiesFn) => {
   console.log('fetch 취미');
   // const response = await axios.get(API_URL + `favorite/list`);
   const response = await axios.get(
