576b46d60b72bf5aee40751efb2ebbee20fb9fbf
eunminbb <mimi0207ej@naver.com>
2022-07-26 21:47:27 +0900

    Feat: fetch followers count and followees count and make follow button

--- frontend/src/features/profile/MyPage.js
+++ frontend/src/features/profile/MyPage.js
@@ -1,11 +1,14 @@
 // // component
-// import Button from '../../../common/UI/Button';
+
 import * as React from 'react';
 
 
 //material UI
-import { Avatar, Chip, Stack, Badge  } from '@mui/material';
+import { Avatar, Chip, Stack, Badge, Card, CardMedia, CardContent, Typography
+  , Button  } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { green, blue, yellow, purple, deepOrange, red } from '@mui/material/colors';
+import { CompareArrows, GTranslate, Group } from '@mui/icons-material';
 
 // component 
 import ProfileImageEdit from './ProfileImageEdit'
@@ -15,6 +18,7 @@ import KoreaFlag from './images/KoreaFlag.png'
 
 // 리덕스 안거치는 단순 서버 통신 API
 import { fetchProfile, fetchDescription, fetchProfileImg } from './ProfileAPI';
+import { fetchFollow } from './FollowAPI';
 
 // state
 import { useState, useRef, useEffect } from 'react';
@@ -30,57 +34,38 @@ import classes from './MyPage.module.scss';
 
 
 
-// const StyledBadge = styled(Badge)(({ theme }) => ({
-//   '& .MuiBadge-badge': {
-//     backgroundColor: '#44b700',
-//     color: '#44b700',
-//     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-//     '&::after': {
-//       position: 'absolute',
-//       top: 0,
-//       left: 0,
-//       width: '100%',
-//       height: '100%',
-//       borderRadius: '50%',
-//       animation: 'ripple 1.2s infinite ease-in-out',
-//       border: '1px solid currentColor',
-//       content: '""',
-//     },
-//   },
-//   '@keyframes ripple': {
-//     '0%': {
-//       transform: 'scale(.8)',
-//       opacity: 1,
-//     },
-//     '100%': {
-//       transform: 'scale(2.4)',
-//       opacity: 0,
-//     },
-//   },
-// }));
-
-
-
 
 
 const MyPage = () => {
-  const API_URL = 'http://127.0.0.1:8080/';
   const params = useParams();
   const [profileInfo, setProfileInfo] = useState(null);
   const [profileImg, setProfileImg] = useState(null);
   const [profileDescription, setProfileDescription] = useState('');
+  const [followCnt, setFollowCnt] = useState({
+    followerCnt: 0, followeeCnt: 0
+  });
   // console.log(params.userId);
+
+  // redux
+  const { currentUser } = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
+  console.log('리덕스 테스트:', currentUser );
+
+  
+  const isCurrentUser = currentUser.id === Number(params.userId);
+  // console.log(isCurrentUser);
 
   useEffect(() => {
     fetchProfile(setProfileInfo, params.userId);
-    // fetchProfileImg(setProfileImg, params.userId);
+    fetchProfileImg(setProfileImg, params.userId);
     fetchDescription(setProfileDescription, params.userId);
-    setProfileImg(API_URL + `image/profile/${params.userId}.jpg`)
+    fetchFollow(setFollowCnt, params.userId);
   }, []);
 
 
   
   const colors = [ "primary", "secondary", "success", "info", "error", "warning" ]
+  // const colors = [ green[400], blue[200], yellow[300], yellow[800], purple[300], purple[600], deepOrange[600], red[300]  ]
   // const chosenColor = colors[Math.floor(Math.random()*colors.length)]
 
   // flag 이모티곤 나중에 설정 하기
@@ -90,48 +75,108 @@ const MyPage = () => {
   // // 의존성에 fetchProfile 추가하면 fetchProfile에 useCallback 함수로.
 
 
+  // 팔로우 되어있으면 true, 아니면 false인 isfollowed도 만들기
+  const [isFollowed, setIsFollowed] = useState(false);
+
+  const followHandler = () => {
+
+  }
+
   return (
     <div>
-      <h1>My Page</h1>
-      {/* currentUser의 프로필페이지에서만 profileImageEdit 가능하게 */}
-      <ProfileImageEdit />
       
-      <div>
-        <Badge
-          badgeContent={
-            <img className={classes.flag} alt="flag" src={KoreaFlag} /> 
-          }
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-          overlap="circular"
-        >
-          <Avatar sx={{ width: 200, height: 200 }} src={profileImg} />    
-        </Badge>
+      {profileInfo && <div> 국적: {profileInfo.nationality}</div>}
+      {/* currentUser의 프로필페이지에서만 profileImageEdit 가능하게 */}
+      <Card className={classes.card} >
+        <div className={classes.left_profile}>
+          <Typography sx={{ color: 'rgba(0, 0, 0, 0.6)', fontSize: 30, fontWeight: 'bold' }} className={classes.header}>My Page</Typography>
+          <CardMedia className={classes.avatar}>
+            <Badge
+              badgeContent={
+                <img className={classes.flag} alt="flag" src={KoreaFlag} /> 
+              }
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+              overlap="circular"
+              >
+              <Avatar sx={{ width: 200, height: 200 }} src={profileImg} />    
+              { isCurrentUser && <ProfileImageEdit/>}
+            </Badge>
+          </CardMedia>
+          
+          <div className={classes.profile_content}>
 
-      </div>
+            {profileInfo && <CardContent >
+              <Typography className={classes.name} 
+                sx={{ fontWeight: 'bold', letterSpacing: 3, fontSize: 26 }} 
+                gutterBottom component="div">
+                {profileInfo.name}
+              </Typography>
 
-      {profileInfo && 
-        <div>
-          <h2>{profileInfo.name}</h2>
-          <h4>{profileInfo.nationality}</h4>
-          <p>{profileDescription}</p>
-          <br />
-          <p>me:  {profileInfo.mylanguage}</p>
-          <p>you:  {profileInfo.yourlanguage}</p>
-          <div>
-            {profileInfo.favorites.map((fav) => {
-              return (
-                <Chip
-                  key={profileInfo.favorites.indexOf(fav)}
-                  label={fav} 
-                  color={colors[Math.floor(Math.random()*colors.length)]}
-                />
-              );
-            })}
+              {/* follow 되어있으면 팔로우 취소도 만들기!!!! */}
+              {!isCurrentUser && <Button onClick={followHandler} variant="contained" 
+                size="small" fullWidth color="secondary"
+                sx={{borderRadius: '25px', letterSpacing: 3 }}>팔로우</Button>}
+
+              <div className={classes.follow}>
+                <Group sx={{  fontSize: 27, mx: 2, color: '#B8C5D0'}} />
+                <div className={classes.followCnt}>
+                  <span className={classes.follow_greys}>팔로워 </span>
+                  <span className={classes.cnt}>{followCnt.followerCnt} </span>
+                  <span className={classes.follow_greys}> 팔로잉 </span>
+                  <span className={classes.cnt}>{followCnt.followeeCnt} </span>
+                </div>
+                
+
+              </div>
+              
+              <Card className={classes.description_card}>
+                <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'light' }}>
+                  {profileDescription}
+                </Typography>
+              </Card>
+              <Card className={classes.rest_info}>
+                <div className={classes.languages}>
+                  <GTranslate sx={{  fontSize: 20, mr: 2, color: '#B8C5D0'}} />
+                  <div>
+                    <span className={classes.language}>{profileInfo.mylanguage} </span>
+                    <span className={classes.greys}>me</span>
+                    <CompareArrows sx={{  fontSize: 20, mx: 1 }} />
+                    <span className={classes.language}> {profileInfo.yourlanguage}</span>
+                    <span className={classes.greys}> you</span>
+
+                  </div>
+
+                </div>
+                <br />
+                <div className={classes.favorite_chips}>
+                  {profileInfo.favorites.map((fav) => {
+                    return (
+                      <Chip
+                        key={profileInfo.favorites.indexOf(fav)}
+                        label={fav} 
+                        color={colors[Math.floor(Math.random()*colors.length)]}
+                        className={classes.chip}
+                      />
+                    );
+                  })}
+                </div>
+
+              </Card>
+
+              
+
+            </CardContent>}
           </div>
+          
+          <div></div>
+    
+
+
         </div>
-      }
+      </Card>
     </div>
   );
 };
 
 export default MyPage;
+
