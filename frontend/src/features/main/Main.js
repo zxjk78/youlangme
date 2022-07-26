91d3cd1a0560442da7d05ff73669450877129f4f
kms940125 <kms940125@hanmail.net>
2022-07-26 13:21:28 +0900

    feat: currentUser issue

--- frontend/src/features/main/Main.js
+++ frontend/src/features/main/Main.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { logout } from "../auth/authSlice";
 const Main = () => {
-  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { isLoggedIn, currentUser } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const history = useHistory();
   const logoutHandler = () => {
     dispatch(logout());
-    history.push("/");
   };
-  console.log(isLoggedIn);
+  console.log(currentUser.id);
+
   // const { user } = useSelector((state) => state.auth);
   // console.log(user);
   return (
     <div>
       <h2>이곳은 임시 홈페이지</h2>
-      <p>{isLoggedIn}</p>
+      <div>
+        <Link to="/board">게시판 작업</Link>
+      </div>
+      <div>
+        <Link to={`/profile/${currentUser.id}`}>프로필 작업</Link>
+      </div>
+      <div>
+        <Link to="/modify">수정</Link>
+      </div>
       <button onClick={logoutHandler}>로그아웃</button>
     </div>
   );
