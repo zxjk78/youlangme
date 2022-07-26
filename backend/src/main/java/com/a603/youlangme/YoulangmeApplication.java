76a9f946f0ff4fabdf869ffa2c2d86284f2e4271
이민호 <jlh369@naver.com>
2022-07-26 22:59:47 +0900

    Feat : Add logging

--- backend/src/main/java/com/a603/youlangme/YoulangmeApplication.java
+++ backend/src/main/java/com/a603/youlangme/YoulangmeApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableJpaAuditing // JPA Auditing을 활성화
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class YoulangmeApplication {
 
 
