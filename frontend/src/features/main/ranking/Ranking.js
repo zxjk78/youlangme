9f0bc43a530b8e51e33fbc4296758b428473927b
noh yongrae <zxjk78@gmail.com>
2022-08-01 08:45:44 +0900

    null ref property

---
+++ frontend/src/features/main/ranking/Ranking.js
@@ -0,0 +1,12 @@
+import classes from './Ranking.module.scss';
+
+const Ranking = (props) => {
+  return (
+    <>
+      <div className={classes.wrapper}></div>
+      <div className={classes.container}></div>
+      <div className={classes.header}></div>
+    </>
+  );
+};
+export default Ranking;
