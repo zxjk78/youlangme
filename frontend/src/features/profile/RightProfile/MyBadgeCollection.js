dd15dad728f633a051154e03bd73c9e9975bec0c
eunminbb <mimi0207ej@naver.com>
2022-08-02 16:51:25 +0900

    Design: Make right side of profile layout and pie chart of language statistics

---
+++ frontend/src/features/profile/RightProfile/MyBadgeCollection.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+
+// mui
+import { Card, CardContent, CardMedia, Typography, CardActionArea } from '@mui/material';
+
+
+// css
+import classes from './RightProfile.module.scss'
+
+const MyBadgeCollection = (props) => {
+  return (
+    <div className={classes.badge_container}>
+
+      {/* 레벨 클릭하면 모달창!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+      <Typography gutterBottom color="#9BA7AF" variant="h6" component="div">
+        내 배지
+      </Typography>
+
+      <Card className={classes.badge_card}  
+        fullWidth
+        sx={{ m:1 }}
+        >
+          <CardContent>
+            <div >
+              <Typography gutterBottom 
+              // color="#FFC700" 
+              variant="h5" component="span">
+                배지리스트 들어갈 자리!
+              </Typography>
+              {/* <Typography gutterBottom color="#9BA7AF" variant="h4" component="span">1</Typography> */}
+            </div>
+          </CardContent>
+      </Card>
+    </div>
+
+  )
+}
+
+
+export default MyBadgeCollection
+
+
--- frontend/src/features/profile/RightProfile/MyLevel.js
+++ frontend/src/features/profile/RightProfile/MyLevel.js
@@ -13,19 +13,19 @@ const MyLevel = (props) => {
     <div className={classes.container}>
 
       {/* 레벨 클릭하면 모달창!!!!!!!!!!!!!!!!!!!!!!!!!! */}
-      <Typography gutterBottom color="#9BA7AF" variant="h5" component="div">
+      <Typography gutterBottom color="#9BA7AF" variant="h6" component="div">
         내 레벨
       </Typography>
 
       <Card className={classes.level_card}  
         // fullWidth
-        sx={{ m:4 }}>
+        sx={{ m:1 }}>
         <CardActionArea>
-          <CardContent sx={{ height: '200px'}}>
-            <EmojiEventsIcon sx={{ fontSize: 80, color: '#FFC700' }} className={classes.trophy} />
-            <div className={classes.level}>
-              <Typography gutterBottom color="#9BA7AF" variant="h3" component="span">Lv.</Typography>
-              <Typography gutterBottom color="#FFC700" variant="h3" component="span">36</Typography>
+          <CardContent sx={{ height: 300}} className={classes.level} >
+            <EmojiEventsIcon sx={{ fontSize: 140, color: '#FFC700' }} className={classes.trophy} />
+            <div >
+              <Typography gutterBottom color="#FFC700" variant="h2" component="span">GOLD</Typography>
+              <Typography gutterBottom color="#9BA7AF" variant="h4" component="span">1</Typography>
             </div>
           </CardContent>
         </CardActionArea>
