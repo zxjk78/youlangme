4f84a5ca660b58caf6cc3fdf0e90357df1c65816
jlh369 <jlh369@naver.com>
2022-08-05 19:04:39 +0900

    Feat: Init matchmkaing server

---
+++ backend/src/main/java/com/a603/youlangme/dto/matching/MatchingRequestDto.java
@@ -0,0 +1,10 @@
+package com.a603.youlangme.dto.matching;
+
+import lombok.Getter;
+
+@Getter
+public class MatchingRequestDto {
+    private String mylanguage;
+    private String yourlanguage;
+}
+
