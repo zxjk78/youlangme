56b91f06b76ef83941f99034823f122bdd58144b
kms940125 <kms940125@hanmail.net>
2022-08-14 17:17:10 +0900

    Fix: Fix login issue

--- frontend/src/features/matching/StartMatching.js
+++ frontend/src/features/matching/StartMatching.js
@@ -1,31 +1,53 @@
 import { useState } from "react";
-import StartChat from "./startMatching,js/StartChat";
-import LoadingChat from "./startMatching,js/LoadingChat";
-import ConfirmChat from "./startMatching,js/ConfirmChat";
+import StartChat from "./beforeMatching/StartChat";
+import LoadingChat from "./beforeMatching/LoadingChat";
+import ConfirmChat from "./beforeMatching/ConfirmChat";
 
 const StartMatching = () => {
   const [matchLoading, setMatchLoading] = useState(false);
   const [matchConfirm, setMatchConfirm] = useState(false);
   const [matching, setMatching] = useState(false);
-  const [sessionId, setSessionId] = useState('')
-  const [opponentId, setOpponentId] = useState(null)
-  const [myData, setMyData] = useState(null)
-  const [yourData, setYourData] = useState(null)
-  const [myProfile, setMyProfile] = useState(null)
-  const [yourProfile, setYourProfile] = useState(null) 
-  console.log(sessionId)
-
+  const [sessionId, setSessionId] = useState("");
+  const [opponentId, setOpponentId] = useState(null);
+  const [myLanguage, setMyLanguage] = useState(null);
+  const [yourLanguage, setYourLanguage] = useState(null);
+  const [myData, setMyData] = useState(null);
+  const [yourData, setYourData] = useState(null);
+  const [myProfile, setMyProfile] = useState(null);
+  const [yourProfile, setYourProfile] = useState(null);
+  console.log(sessionId);
 
   if (!matchLoading && !matchConfirm) {
-    return ( <StartChat setMatchLoading={setMatchLoading} setMatchConfirm={setMatchConfirm} setSessionId={setSessionId} setOpponentId={setOpponentId}></StartChat>
+    return (
+      <StartChat
+        setMyLanguage={setMyLanguage}
+        setYourLanguage={setYourLanguage}
+        setMatchLoading={setMatchLoading}
+        setMatchConfirm={setMatchConfirm}
+        setSessionId={setSessionId}
+        setOpponentId={setOpponentId}
+      ></StartChat>
     );
   } else if (matchLoading) {
+    return <LoadingChat></LoadingChat>;
+  } else if (!matchLoading && matchConfirm) {
     return (
-      <LoadingChat></LoadingChat>
+      <ConfirmChat
+        myLanguage={myLanguage}
+        yourLanguage={yourLanguage}
+        sessionId={sessionId}
+        setMatching={setMatching}
+        setMyData={setMyData}
+        setYourData={setYourData}
+        setMyProfile={setMyProfile}
+        setYourProfile={setYourProfile}
+        matchConfirm={matchConfirm}
+        setMatchConfirm={setMatchConfirm}
+        setSessionId={setSessionId}
+        setOpponentId={setOpponentId}
+        opponentId={opponentId}
+      ></ConfirmChat>
     );
-  } else if (!matchLoading && matchConfirm ) {
-    return ( <ConfirmChat sessionId={sessionId} setMatching={setMatching} setMyData={setMyData} setYourData={setYourData} setMyProfile={setMyProfile} setYourProfile={setYourProfile}  matchConfirm={matchConfirm} setMatchConfirm={setMatchConfirm} setSessionId={setSessionId} setOpponentId={setOpponentId} opponentId={opponentId}></ConfirmChat>
-    );
-  } 
+  }
 };
 export default StartMatching;
---
+++ frontend/src/features/matching/beforeMatching/ConfirmChat.js
@@ -0,0 +1,212 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import axios from "axios";
+import {
+  fetchProfile,
+  fetchProfileImg,
+} from "../../profile/LeftProfile/LeftProfileAPI";
+
+// data import
+import { API_URL, accessToken } from "../../../common/api/http-config";
+import { iso_code } from "../../profile/LeftProfile/UserInfo/flagData";
+
+// mui
+import { Avatar, Badge, Button, CircularProgress } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
+
+// css
+import classes from "./StartMatch.module.scss";
+import { grey } from "@mui/material/colors";
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  // color: theme.palette.getContrastText(yellow[500]),
+  fontSize: 30,
+  fontWeight: 1000,
+  lineHeight: 1.5,
+  padding: "20px 20px",
+  margin: "30px 0px 20px 0px",
+  width: 380,
+
+  border: "1px solid #CAD6E2",
+  borderRadius: 20,
+  backgroundColor: "#B865C6",
+  "&:hover": {
+    backgroundColor: "#EC7EFF",
+  },
+}));
+
+const ConfirmChat = (props) => {
+  const history = useHistory();
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const [MyInfo, setMyInfo] = useState(null);
+  const [youInfo, setYouInfo] = useState(null);
+  const [myImage, setMyImage] = useState(null);
+  const [yourImage, setYourImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (props.matchConfirm) {
+      setTimeout(() => {
+        props.setOpponentId(null);
+        props.setSessionId("");
+        props.setMyData(null);
+        props.setYourData(null);
+        props.setMyProfile(null);
+        props.setYourProfile(null);
+        props.setMatchConfirm(false);
+      }, 25000);
+    }
+    return () => {
+      setMyInfo(null);
+      setMyImage(null);
+      setYouInfo(null);
+      setYourImage(null);
+    };
+  }, [props.matchConfirm]);
+
+  const matchingHandler = () => {
+    axios
+      .post(
+        API_URL + `meeting/enter/${props.sessionId}`,
+        { yourlanguage: MyInfo.yourlanguage },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-AUTH-TOKEN": accessToken,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        history.push({
+          pathname: "/match",
+          state: {
+            sessionId: props.sessionId,
+            MyInfo: MyInfo,
+            youInfo: youInfo,
+          },
+        });
+      })
+      .catch((err) => console.log(err.message));
+  };
+
+  useEffect(() => {
+    (async () => {
+      if (props.matchConfirm) {
+        setIsLoading(true);
+        const data = await fetchProfile(currentUser.id);
+        if (!data) {
+          history.replace({
+            pathname: "/404",
+            message: "존재하지 않는 게시물입니다.",
+          });
+        }
+        const dataImage = await fetchProfileImg(currentUser.id);
+        setMyInfo(data);
+        setMyImage(dataImage);
+        props.setMyData(data);
+        props.setMyProfile(dataImage);
+        setIsLoading(false);
+      }
+    })();
+  }, [props.matchConfirm, currentUser]);
+
+  useEffect(() => {
+    (async () => {
+      if (props.matchConfirm) {
+        setIsLoading(true);
+        const data1 = await fetchProfile(props.opponentId);
+        const data1Image = await fetchProfileImg(props.opponentId);
+        setYouInfo(data1);
+        setYourImage(data1Image);
+        props.setYourData(data1);
+        props.setYourProfile(data1Image);
+        setIsLoading(false);
+      }
+    })();
+  }, [props.matchConfirm, props.opponentId]);
+
+  const myNationalityCode = MyInfo ? iso_code[MyInfo.nationality] : null;
+  const yourNationalityCode = youInfo ? iso_code[youInfo.nationality] : null;
+  // console.log(myNationalityCode, yourNationalityCode);
+
+  return (
+    <>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <div className={classes.match_wrapper}>
+          <div className={classes.confirm_ment}>매칭 성공!</div>
+          <div className={classes.matching_pair}>
+            {MyInfo && myImage && (
+              <div className={classes.match_user}>
+                <Badge
+                  badgeContent={
+                    <img
+                      className={classes.flag}
+                      alt="flag"
+                      src={`http://www.geonames.org/flags/x/${myNationalityCode}.gif`}
+                    />
+                  }
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                  overlap="circular"
+                >
+                  <Avatar sx={{ width: 250, height: 250 }} src={myImage} />
+                </Badge>
+                <div className={classes.match_user_name}>{MyInfo.name}</div>
+              </div>
+            )}
+
+            {youInfo && yourImage && (
+              <div className={classes.match_user}>
+                <Badge
+                  badgeContent={
+                    <img
+                      className={classes.flag}
+                      alt="flag"
+                      src={`http://www.geonames.org/flags/x/${yourNationalityCode}.gif`}
+                    />
+                  }
+                  anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                  overlap="circular"
+                >
+                  <Avatar sx={{ width: 250, height: 250 }} src={yourImage} />
+                </Badge>
+                <div className={classes.match_user_name}>{youInfo.name}</div>
+              </div>
+            )}
+          </div>
+          {MyInfo && youInfo && (
+            <div className={classes.match_success_lngs}>
+              <div
+                className={`${classes.match_success_each_lng} ${classes.custom_text_grey}`}
+              >
+                {props.myLanguage}
+              </div>
+              <SwapHorizIcon sx={{ fontSize: 40, mx: 3, mt: "1px" }} />
+              <div
+                className={`${classes.match_success_each_lng} ${classes.custom_text_yellow}`}
+              >
+                {props.yourLanguage}
+              </div>
+            </div>
+          )}
+
+          <ColorButton
+            variant="contained"
+            sx={{ mt: 3 }}
+            onClick={matchingHandler}
+          >
+            입장하기
+          </ColorButton>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ConfirmChat;
