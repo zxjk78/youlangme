853d61c6cbdd584dc0c9b6df307d56e3ea6b17b8
zxjk78 <zxjk78@gmail.com>
2022-08-18 16:00:24 +0900

    Fix: Fix clock

--- frontend/src/common/utils/functions/commonFunctions.js
+++ frontend/src/common/utils/functions/commonFunctions.js
@@ -4,11 +4,13 @@ import 'moment/locale/ko';
 // 날짜 데이터 받아서 방금 전, 몇 시간 전, 하루 넘어가면 Date text로 변환하는 함수
 export const createdDateCal = (createdDate, isDetail = true) => {
   if (isDetail) {
-    const created = moment(createdDate).format('YYYY-MM-DD HH:mm');
+    const created = moment(createdDate)
+      .add(9, 'hours')
+      .format('YYYY-MM-DD HH:mm');
     return created;
   } else {
     // 요약 피드에서 보일 때 ~ 분 ~ 시간 전 ... 날짜
-    const m = moment(createdDate);
+    const m = moment(createdDate).add(9, 'hours');
     // 1일 이상 밀리면 어제, 몇시 로
 
     if (m.isBefore(moment().subtract(1, 'days'))) {
